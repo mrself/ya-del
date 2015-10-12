b30c9466add32a92a8386b05a9ce6a9432465a19
Lightman <yaruk.my@gmail.com>
2015-10-12 16:41:12 +0300

    Refactore. Add npm test

---
+++ index.test.js
@@ -0,0 +1,11 @@
+var chai = require('chai'),
+	expect = chai.expect,
+	assert = chai.assert,
+	$ = require('jquery'),
+	del = require('./del');
+
+describe('#makeName', function() {
+	it('it should work', function() {
+		assert(true);
+	});
+});
